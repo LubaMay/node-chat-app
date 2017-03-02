2c40e0c2bb92ab351fd1c10425aa44773ea1a43f
Lubov Mayorova <Liubov.Mayorova@gmail.com>
2017-03-01 19:08:02 -0800

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+    var socket = io();
+
+    socket.on('connect', function () {
+        console.log('Connected to server');
+
+
+        socket.emit('createMessage', {
+            from: 'Chat userOne',
+            text: 'Hello there! I am here :)'
+        });
+    });
+
+
+
+    socket.on('disconnect', function () {
+        console.log('Disconnected from server');
+    });
+
+
+    socket.on('newMessage', function (message) {
+       console.log('newMessage', message);
+    });
